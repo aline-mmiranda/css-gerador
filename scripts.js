391109bbd0abb5e797b375e0b216263d6cd53176
rodolfomori <rod_dolpho@hotmail.com>
2026-02-25 18:42:42 -0300

    Aula 2

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/* 
+    Variável - Pedacinho de memória
+    que eu posso guardar o que eu quiser
+
+
+*/
+
+let nome = "Rodolfo"
+let sobrenome = "Mori"
+
+console.log(nome)
+console.log(sobrenome)
