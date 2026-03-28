05676777cb3d455c549c22d3ab28063bf407d93c
aline-mmiranda <contato.alinemmiranda@gmail.com>
2026-03-28 09:26:51 -0300

    adicionando arquivos

--- scripts.js
+++ scripts.js
@@ -1,50 +1,19 @@
-/* 
-    Variável - Pedacinho de memória
-    que eu posso guardar o que eu quiser
+import { apiKey } from './config.js';
 
-    Função - Pedacinho de código QUE só EXECUTA
-    Quando é chamado
-
-    Algoritmo - Receita do Bolo
-    Lógica de Programação -  Fazer o bolo
-
-    // Algoritmo do nosso sistema
-    // Lógica de programação
-
-    [x] Saber quem é o botão
-    [x] Saber quando o botão foi clicado
-    [x] Saber quem é o textarea  
-    [x] Pegar o que tem dentro dele
-    [x] Enviar para a IA
-    [x] Pegar a resposta da IA e colocar na tela 
-    [ ] Estilizar a resposta     
-
-    // Ir no HTML e pegar o botão
-    // HTML = document (documento)
-    // Selecionar (querySelector)
-    // Quem ? Botão
-    // Apelido para o botão - classes(class) = .
-    fetch - ferramenta do JS para se comunicar com o servidor
-*/
-
-// Descobri que é o botao
-let botao = document.querySelector(".botao-gerar")
-let endereco = "https://api.groq.com/openai/v1/chat/completions"
-
-// Criei a funcao que será chamada quando clicar 
-// no botao
+let botao = document.querySelector(".botao-gerar");
+let endereco = "https://api.groq.com/openai/v1/chat/completions";
 
 async function gerarCodigo() {
 
-    let textoUsuario = document.querySelector(".caixa-texto").value
-    let blocoCodigo = document.querySelector(".bloco-codigo")
-    let resultadoCodigo = document.querySelector(".resultado-codigo")
+    let textoUsuario = document.querySelector(".caixa-texto").value;
+    let blocoCodigo = document.querySelector(".bloco-codigo");
+    let resultadoCodigo = document.querySelector(".resultado-codigo");
 
     let resposta = await fetch(endereco, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-            "Authorization": "Bearer <key aqui>"
+            "Authorization": `Bearer ${apiKey.GROQ_API_KEY}`
         },
         body: JSON.stringify({
             model: "llama-3.3-70b-versatile",
@@ -59,22 +28,14 @@ async function gerarCodigo() {
                 }
             ]
         })
-    })
+    });
 
-    let dados = await resposta.json()
-    let resultado = dados.choices[0].message.content
+    let dados = await resposta.json();
+    let resultado = dados.choices[0].message.content;
 
-    blocoCodigo.textContent = resultado
-    resultadoCodigo.srcdoc = resultado
+    blocoCodigo.textContent = resultado;
+    resultadoCodigo.srcdoc = resultado;
 
 }
 
-// ficar de olho no botao, quando clicado chamar o gerarCodigo
-botao.addEventListener("click", gerarCodigo)
-
-
-// vizinho curioso (addEventListener)
-// adicionar ouvinte de eventos
-// Evento = clique, digitei...
-
-
+botao.addEventListener("click", gerarCodigo);
